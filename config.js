870d22905d77813e0e0f53053df2b2874664c17d
SvetlanaLanaLana <svpodols@mail.ru>
2026-06-08 21:57:47 +0300

    настройка бота

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Укажите ваш Telegram chat_id (узнать: напишите @userinfobot или /start боту и проверьте getUpdates)
 window.TG_CONFIG = {
   botToken: '8551650843:AAHwEq0fdYNLC1NRk-CqAO2HrLL9oeAGsBc',
-  chatId: '8551650843'
+  chatId: '5232226311'
 };
